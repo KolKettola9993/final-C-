8385b131632e547ccbc3ca4fde91622825264d0e
Soksovanraksmey9794 <sovanraksmey9794@acledaaib.onmicrosoft.com>
2026-06-22 09:58:57 +0700

    updated

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,7 +31,18 @@
             uiTabControlMenu1 = new Sunny.UI.UITabControlMenu();
             tabPage1 = new TabPage();
             tabPage2 = new TabPage();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
             uiTabControlMenu1.SuspendLayout();
+            tabPage1.SuspendLayout();
             SuspendLayout();
             // 
             // uiTabControlMenu1
@@ -51,6 +62,16 @@
             // 
             // tabPage1
             // 
+            tabPage1.Controls.Add(textBox4);
+            tabPage1.Controls.Add(textBox3);
+            tabPage1.Controls.Add(textBox2);
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Controls.Add(button3);
+            tabPage1.Controls.Add(button2);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(label1);
             tabPage1.Location = new Point(201, 0);
             tabPage1.Name = "tabPage1";
             tabPage1.Size = new Size(1144, 648);
@@ -67,6 +88,88 @@
             tabPage2.Text = "tabPage2";
             tabPage2.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(244, 76);
+            label1.Name = "label1";
+            label1.Size = new Size(79, 29);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(244, 252);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 29);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(244, 157);
+            label3.Name = "label3";
+            label3.Size = new Size(79, 29);
+            label3.TabIndex = 2;
+            label3.Text = "label3";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(244, 303);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(244, 384);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 4;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(244, 463);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 5;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(497, 157);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 35);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(497, 265);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 35);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(497, 360);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(150, 35);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(497, 462);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(150, 35);
+            textBox4.TabIndex = 9;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
@@ -76,6 +179,8 @@
             Name = "Form1";
             Text = "Form1";
             uiTabControlMenu1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -84,5 +189,15 @@
         private Sunny.UI.UITabControlMenu uiTabControlMenu1;
         private TabPage tabPage1;
         private TabPage tabPage2;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
     }
 }
